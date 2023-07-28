c7cd6064f27e600eacf7366ad6743173848db319
jonnyACCI <jshan9078@gmail.com>
2023-07-27 23:04:21 -0400

    Adding data scraper

---
+++ gogoscraper.py
@@ -0,0 +1,52 @@
+from bs4 import BeautifulSoup
+import requests
+
+class Anime:
+    def __init__(self, siteLink, title, img, release, seriesID):
+        self.siteLink = siteLink
+        self.title = title
+        self.img = img
+        self.release = release
+        self.seriesID = seriesID
+
+    def display(self):
+        print(self.title)
+        print(f"Poster Image: {self.img}")
+        print(f"Watch Link: {self.siteLink}")
+        print(f"Series ID: {self.seriesID}")
+        print(f"Release Year: {self.release}")
+        print()
+
+def displayAll(choices):
+    for i in choices:
+        i.display()
+
+wantedAnime = "naruto"
+
+html_content = requests.get("https://gogoanime3.net/search.html?keyword="+wantedAnime).text
+soup = BeautifulSoup(html_content, "lxml")
+searchResults = soup.find("ul", class_="items")
+rawSources = searchResults.find_all("a")
+rawDates = searchResults.find_all("p",class_="released")
+dates=[]
+sources = []
+choices = []
+for i in rawDates:
+    dates.append(i.text.split('Released: ')[1].split(' ')[0])
+for i in range(len(rawSources)):
+    if (i%2==0):
+        sources.append(rawSources[i])
+for i in range(len(sources)):
+    title = sources[i].get("title")
+    linkStem = sources[i].get("href")
+    seriesLink = "https://gogoanime3.net"+linkStem
+    seriesID = linkStem.split('category/')[1]
+    img = sources[i].find("img").get('src')
+    releaseDate = dates[i]
+    animeSeries = Anime(seriesLink,title,img,releaseDate,seriesID)
+    choices.append(animeSeries)
+displayAll(choices)
+
+
+    
+
